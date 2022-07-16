b81d671687271f8298933c9b670eba54524afa74
Furkan Salim <furkansalimm@gmail.com>
2022-07-16 13:47:07 +0300

    create custom methods to query db

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
+
+  def self.recent_comments(comments)
+    comments.order(created_at: :desc).limit(5)
+  end
+
+  def self.update_post_counter(author)
+    author.increment!(:postscounter)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_many :comments
   has_many :likes
   has_many :posts
+
+  def self.recent_posts(posts)
+    posts.order(created_at: :desc).limit(3)
+  end
 end
