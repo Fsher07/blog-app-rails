01363e3662b3e660fb69802d0f623024b35d35ff
Furkan Salim <furkansalimm@gmail.com>
2022-07-22 14:52:18 +0300

    create layouts and templates

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,10 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @user = User.find(params[:user_id])
+    @posts = @user.posts
   end
 
   def show
-    @posts = Post.find(params[:id])
+    @post = Post.find(params[:id])
   end
 end
