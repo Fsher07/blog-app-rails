4c82fcf9df8c41a6a26adbc434905c3147365307
Furkan Salim Hıdır <furkansalimm@gmail.com>
2022-07-22 18:13:34 +0300

    Revert "Views"

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,8 +7,6 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
-  after_save :update_post_counter
-
   def recent_comments
     comments.order(created_at: :desc).limit(5)
   end
