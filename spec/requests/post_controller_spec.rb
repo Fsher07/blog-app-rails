6511bace96332638d1432e7204733ad1117726f0
Furkan Salim <furkansalimm@gmail.com>
2022-07-22 15:44:02 +0300

    Change user id to pass test

--- spec/requests/post_controller_spec.rb
+++ spec/requests/post_controller_spec.rb
@@ -2,7 +2,7 @@ require_relative '../rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   describe 'GET /index' do
-    before(:example) { get('/users/4/posts') }
+    before(:example) { get('/users/5/posts') }
     it 'http status code should be 200' do
       expect(response).to have_http_status(200)
     end
@@ -15,7 +15,7 @@ RSpec.describe 'Posts', type: :request do
   end
 
   describe 'GET /show' do
-    before(:example) { get('/users/4/posts/7') }
+    before(:example) { get('/users/5/posts/15') }
     it 'http status code should be 200' do
       expect(response).to have_http_status(200)
     end
